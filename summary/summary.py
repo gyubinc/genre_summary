39bbb199d04693e9c0cba95b7bbb82e39f2e3a3f
gyubin <yeppi315@gmail.com>
2023-07-15 16:55:17 +0000

    fix) summary.py

--- summary/summary.py
+++ summary/summary.py
@@ -1,6 +1,7 @@
 def summary(tokenizer, model, passage):
-    input_ids = tokenizer.encode(passage, return_tensors='pt', add_special_tokens=True)
-    outputs = model.generate(input_ids=input_ids, num_beams=8)
+    input_ids = tokenizer.encode(passage, return_tensors='pt', add_special_tokens=True, truncation=True)
+    #outputs = model.generate(input_ids=input_ids, num_beams=8, length_penalty=0.8, max_length=128)
+    outputs = model.generate(input_ids=input_ids, num_beams=8, length_penalty=1.0, max_length=128)
     decoded_summary = tokenizer.decode(outputs.squeeze(), skip_special_tokens=True)
     return decoded_summary
 
