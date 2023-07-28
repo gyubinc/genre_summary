8f82d22cbf0b6ac6f33430afaa6efec33c170977
gyubin <yeppi315@gmail.com>
2023-07-28 07:47:35 +0000

    fix) 실행 코드

--- mode.py
+++ mode.py
@@ -12,7 +12,32 @@ model = BertForSequenceClassification.from_pretrained('gyubinc/bert-book32-gyubi
 
 if __name__ == "__main__":
     # predict_genre()
-    text = "He died yesterday"
+    text = """
+    This night, the fireflies of the day are yours
+    I'll send it near the window
+    I mean I love you
+    I remember our first kiss
+    So anytime close your eyes
+    go to the furthest place
+    I was a wave
+    Like letters written in the sand
+    I think you will disappear far away
+    I always miss you, miss you
+    all the words here in my heart
+    I can't take it all out
+    I mean I love you
+    how to me
+    Have you been lucky?
+    if we are together now
+    oh how nice
+    I was a wave
+    Like letters written in the sand
+    I think you will disappear far away
+    I miss you again, I miss you more
+    all the words in my diary
+    I can't take it all out
+    saying I love you
+    """
 
     # 텍스트의 장르 예측
     music_genre = predict_genre(text, model, tokenizer, le, k=1)  # 'le'를 사용하셔야 합니다.
